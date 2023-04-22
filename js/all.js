ea7de013a46a1ed6120d149c013bf8c6a891d976
aria198a <aria198a@gmail.com>
2023-04-22 20:12:35 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    // dropdown menu
+    $('.menu .dropdown').click(function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.siblings('.submenu').slideToggle(400);
+        setTimeout(function() {
+            this.toggleClass('active');
+        }, 400);
+        this.parent().siblings().find('.submenu').slideUp(400);
+        setTimeout(function() {
+            this.parent().siblings().children('.dropdown').removeClass('active');
+        }, 400);
+    });
+    $('html').click(function(event) {
+        $('.submenu').slideUp(400);
+        setTimeout(function() {
+            $('.dropdown').removeClass('active');
+        }, 400);
+    });
+
+    // swiper
+    var mySwiper = new Swiper('.banner', {
+        speed: 500,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        
+    });
+
+    // lightbox
+    lightbox.option({
+        showImageNumberLabel: false,
+        positionFromTop: 100,
+    })
+    $('.course>ul>li a').click(function(event) {
+        /* Act on the event */
+        $(this).children('h2, p');
+    });
+
+    // top
+    $('.top').click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: 0 }, 500);
+    });
+});
